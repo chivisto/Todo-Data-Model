163a4e36ad10a3acd454db6c7abf62b1765e0a36
Chivisto <isaaccovarr@gmail.com>
2022-09-02 23:06:09 -0600

    Added a function

---
+++ app.js
@@ -0,0 +1,15 @@
+// On form submit add task
+document.querySelector("form").addEventListener("submit", e => {
+    e.preventDefault();
+    addTask();
+});
+
+function addTask() {
+    const task = document.querySelector("form input");
+    const list = document.querySelector("ul");
+    // return if task is empty
+    if (task.value === "") {
+        alert("Add a task");
+        return false;
+    }
+}
