a1ff13b3d1b286ef8b3f44d46809edcd7f94fcbf
Chivisto <isaaccovarr@gmail.com>
2022-12-06 19:47:29 -0700

    Adding consts from og file

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+const uncompletedTodosListEl = document.getElementById("uncompleted-todos-list");
+
+const completedTodosListEl = document.getElementById("completed-todos-list");
+
+const categoriesListEl = document.getElementById("categories-list");
+
+const categoryCreateForm = document.getElementById("category-create-form");
+
+const categoryNameInput = document.getElementById("category-name-input");
+
+const categoryFilterEl = document.querySelector(".categoryFilter");
+
+const categorySelectEl = document.querySelector(".categorySelect");
+
+const createTodoBtn = document.querySelector(".createTodo");
+
+const todoNameInputEl = document.querySelector(".todo-name-input");
+
+const pendingEl = document.querySelector(".pending");
+
+const clearDoneBtn = document.querySelector(".clearTodo");
