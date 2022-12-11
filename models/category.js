7736732fa33643c98617153a5d3aac7fb9822d48
Chivisto <isaaccovarr@gmail.com>
2022-12-10 19:10:43 -0700

    Created category model

---
+++ models/category.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require("mongoose");
+
+const CategorySchema = new Schema({
+    categoryName: {
+        type: String,
+        required: true
+    }
+});
+
+const Category = model("Category", CategorySchema);
+
+module.exports = Category;
